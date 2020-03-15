10d1c3b762f2692eda7ea34d718e61488cf595e0
Blake Holt <blakeholt@blakeholt.net>
2020-03-15 15:23:31 -0300

    Optimized date function

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 // Thanks W3 Schools
-var day;
+
 
 /*var time;
 function startTime() {
@@ -28,65 +28,23 @@ function checkTime(i) {
 }*/
 
 function checkDate() {
-    var td = new Date();
-    var y = td.getFullYear();
-    var m = td.getMonth();
-    var D = td.getDate();
-    var d = td.getDay();
+	let day;
+    let td = new Date();
+    let y = td.getFullYear();
+    let m = td.getMonth();
+    let D = td.getDate();
+    let d = td.getDay();
+	
+	weekday = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
+    months = new Array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
+	domEnder = function() {
+		let a = td;
+		if (/1/.test(parseInt((a + "").charAt(0))))
+			return "th"; a = parseInt((a + "").charAt(1)); 
+		return 1 == a ? "st" : 2 == a ? "nd" : 3 == a ? "rd" : "th" 
+	}();
+	day = weekday[d] + " " + months[m] + " " + D  + domEnder + ", " + y;
 
-    if(d == 0){
-       day = "Sunday, ";
-    }else if(d == 1){
-       day = "Monday, ";
-    }else if(d == 2){
-       day = "Tuesday, ";
-    }else if(d == 3){
-       day = "Wednesday, ";
-    }else if(d == 4){
-       day = "Thursday, ";
-    }else if(d == 5){
-       day = "Friday, ";
-    }else if(d == 6){
-       day = "Saturday, ";
-    }
-
-    if(m == 0){
-       day += "January ";
-    }else if(m == 1){
-       day += "February ";
-    }else if(m == 2){
-       day += "March ";
-    }else if(m == 3){
-       day += "April ";
-    }else if(m == 4){
-       day += "May";
-    }else if(m == 5){
-       day += "June ";
-    }else if(m == 6){
-       day += "July ";
-    }else if(m == 7){
-       day += "August ";
-    }else if(m == 8){
-       day += "September ";
-    }else if(m == 9){
-       day += "October ";
-    }else if(m == 10){
-       day += "November ";
-    }else if(m == 11){
-       day += "December ";
-    }
-
-    day += (D);
-
-    if(day.endsWith("1") && !day.endsWith("11")){
-       day += "st";
-    }else if(day.endsWith("2")){
-       day += "nd";
-    }else if(day.endsWith("3")){
-       day += "rd";
-    }else{
-        day += "th";
-    }
     document.getElementById('date').innerHTML = day;
 }
 
