20cda817e2a60cd65f968595cfc1a81434f5b487
Blake Holt <43155492+blakeholt@users.noreply.github.com>
2021-04-23 22:41:12 -0300

    Update main.js

--- test/main.js
+++ test/main.js
@@ -1,23 +1,10 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+window.onload = () => {
+    document.getElementById("clickme").addEventListener("click", () => speak('Get fucked Connor'));
+}
 
-
-window.onload = function () {
-    let url = "get.php";
-    let xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function () {
-        if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
-            let resp = xmlhttp.responseText;
-            if (resp.search("ERROR") >= 0) {
-                alert("oh no...");
-            } else {
-                document.querySelector("h1").innerHTML = xmlhttp.responseText;
-            }
-        }
-    };
-    xmlhttp.open("GET", url, true);
-    xmlhttp.send();
+function speak(message) {
+    var msg = new SpeechSynthesisUtterance(message)
+    var voices = window.speechSynthesis.getVoices()
+    msg.voice = voices[0]
+    window.speechSynthesis.speak(msg)
 }
