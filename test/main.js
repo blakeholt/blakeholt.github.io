a6e0b4fd5e8e2c1f31e0da4d5c9ae3932fdc8575
Blake Holt <43155492+blakeholt@users.noreply.github.com>
2021-04-23 22:45:02 -0300

    Update main.js

--- test/main.js
+++ test/main.js
@@ -5,6 +5,7 @@ window.onload = () => {
 function speak(message) {
     var msg = new SpeechSynthesisUtterance(message)
     var voices = window.speechSynthesis.getVoices()
+    console.log(voices)
     msg.voice = voices[0]
     window.speechSynthesis.speak(msg)
 }
