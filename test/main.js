3ff140459bae1a3d03a71e8bba75e4cb9bcc8e78
Blake Holt <43155492+blakeholt@users.noreply.github.com>
2021-04-23 22:51:30 -0300

    Update main.js

--- test/main.js
+++ test/main.js
@@ -6,6 +6,6 @@ function speak(message) {
     var msg = new SpeechSynthesisUtterance(message)
     var voices = window.speechSynthesis.getVoices()
     console.log(voices)
-    msg.voice = voices[0]
+    msg.voice = voices[Math.floor(Math.random() * 21)]
     window.speechSynthesis.speak(msg)
 }
